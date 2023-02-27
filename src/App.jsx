f0dbdd6ba75b6bbf73da62e8cf87a2c4812ff087
agustin <agustinsanchezx@gmail.com>
2023-02-27 15:59:28 -0300

    add proyecto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1 className='font-bold underline text-2xl flex justify-center'>Futuro carrito de compras con react y maquetacion en tailwind.css</h1>
+  )
+}
+
+export default App
